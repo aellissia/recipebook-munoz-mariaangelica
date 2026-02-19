9e9407acc1e95a0e479262ff06f7164aa54b00ba
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-02-19 15:11:30 +0800

    Added views and template htmls.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,7 +5,6 @@ app_name = "ledger"
 
 urlpatterns = [
     path('recipes/list', views.recipes_list, name='recipes_list'),
-    path('recipe/1', views.recipe_1, name='recipe_1'),
-    path('recipe/2', views.recipe_2, name='recipe_2'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail')
 ]
 
