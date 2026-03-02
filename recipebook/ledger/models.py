f7fa4a56789e0574dc36fe197f2d1a788df95407
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-03-02 08:47:09 +0800

    Added Profile and modified Recipe to contain author and date.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
@@ -13,6 +23,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="recipes", null=True, blank=True)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
